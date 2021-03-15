ce52492b905ffa4620cc2d1d07d298eaa3ccb024
polyrain <polyrain@users.noreply.github.com>
2021-03-15 06:50:33 +0000

    jekyll build from Action c09311e3ab745387c1fbd58e102f533a95dcd79b

---
+++ js/titles.js
@@ -0,0 +1,5 @@
+
+var titles = ['c > java', 'rain is life', 'dev at it\'s worst', 'don\'t look at bugs', 'now with more cake', 'rainy skies == best', 'now with more bugs!'];
+var title = titles[Math.floor(Math.random() * titles.length)]; // pick random item
+document.title = 'polyrain.net: ' + title;
+
---
+++ upload_posts.sh
@@ -0,0 +1,3 @@
+git add ./_posts/*.md
+git commit -m "Uploaded new blog posts!"
+git push
